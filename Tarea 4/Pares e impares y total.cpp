0b71cd19212c359c340f93c0b95dd3615658917f
Luis Rápalo <98724435+PJyout@users.noreply.github.com>
2022-02-15 20:29:44 -0600

    Add files via upload

---
+++ Tarea 4/Pares e impares y total.cpp
@@ -0,0 +1,41 @@
+/*
+   Proposito: Ejercicio 25 - Suma de pares e impares y total - Tarea 4
+
+   Autor: Luis Alonzo Rápalo
+   Fecha: 15/2/2022
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Spanish");
+	cout.precision(12);
+  
+    int pares = 0;
+    int impares = 0;
+    int total = 0;
+    
+    for (int i = 1; i <= 10; i++)
+    {
+    	if (i % 2 == 0 ){
+    		pares = pares + i;		
+		}
+    	else
+		   impares = impares + i;				
+	}
+	
+	total = pares + impares;
+	
+	cout << " La suma de los pares es: " << pares << endl;
+	cout << " La suma de los impares es: " << impares << endl;   
+    cout << " La suma de ambos es: " << total << endl;		
+
+
+
+	return 0;
+}
+
